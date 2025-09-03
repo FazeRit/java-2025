7534980f7ced7d7b3472adc450f9cd4658fd1b01
DmytroHalai <dimagalai04@gmail.com>
2025-09-03 14:17:35 +0300

    feat: Added application with output
     The output is made by order, fixed with a help of Spring @Order() annotation

---
+++ 1/dmytro-halai/SpringApplicationConsoleHelloWorldByOrder/src/test/java/com/example/springapplicationconsolehelloworldbyorder/SpringApplicationConsoleHelloWorldByOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springapplicationconsolehelloworldbyorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApplicationConsoleHelloWorldByOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

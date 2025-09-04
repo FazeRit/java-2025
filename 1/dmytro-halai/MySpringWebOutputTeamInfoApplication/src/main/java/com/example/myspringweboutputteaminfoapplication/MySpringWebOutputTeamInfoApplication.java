4208800844481551a0ee6876647e3648798625c2
DmytroHalai <dimagalai04@gmail.com>
2025-09-05 01:38:19 +0300

    feat: Added web output of team info

---
+++ 1/dmytro-halai/MySpringWebOutputTeamInfoApplication/src/main/java/com/example/myspringweboutputteaminfoapplication/MySpringWebOutputTeamInfoApplication.java
@@ -0,0 +1,13 @@
+package com.example.myspringweboutputteaminfoapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringWebOutputTeamInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringWebOutputTeamInfoApplication.class, args);
+    }
+
+}

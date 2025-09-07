185e4417b50cbf10cd4a74321481f118f9c10dad
denysHutsan <denys.hut@cgsteam.io>
2025-09-07 21:07:57 +0300

    feat: Added getting of lost item

--- 2/denys-hutsan/lab-2/src/main/java/com/example/lab2/common/interfaces/IAbstractRepository.java
+++ 2/denys-hutsan/lab-2/src/main/java/com/example/lab2/common/interfaces/IAbstractRepository.java
@@ -2,6 +2,9 @@ package com.example.lab2.common.interfaces;
 
 import com.example.lab2.common.entity.AbstractEntity;
 
+import java.util.List;
+
 public interface IAbstractRepository <TItem extends AbstractEntity>{
     public void create(TItem t);
+    public List<TItem> getEntities(TItem... t);
 }
